d18da94e506f7bb5fd1962d4002baf0b3231d590
MattTurnip <mattadavey@gmail.com>
2019-03-06 19:05:59 +0000

    count letters function created

---
+++ count-letters.js
@@ -0,0 +1,24 @@
+function countLetters(string){
+  var splitStr = string.split(" ").join("");
+  var object = {};
+
+
+  for (var i = 0; i < splitStr.length ; i++) {
+
+    if (object[splitStr[i]] === undefined){
+    object[splitStr[i]] = 1
+  }
+  else {
+    object[splitStr[i]] += 1
+  }
+
+  }
+  return object;
+}
+
+
+
+//test parameter
+console.log(countLetters("hello there"));
+
+
